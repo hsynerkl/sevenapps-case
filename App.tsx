55c5aa341f392d31a03d60f2837e923d1b6eed0d
hsynerkl <hsynerkl@icloud.com>
2024-11-28 13:27:08 +0300

    refactor: fixed safeareview with android

--- App.tsx
+++ App.tsx
@@ -1,23 +1,46 @@
 import "./global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView } from "react-native";
+import {
+  SafeAreaView,
+  Platform,
+  StatusBar as RNStatusBar,
+  StatusBarProps,
+} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import RickAndMorty from "@/components/RickAndMorty";
 
 const queryClient = new QueryClient();
 
+interface CustomStatusBarProps extends StatusBarProps {
+  customBackgroundColor?: string;
+}
+
+const CustomStatusBar: React.FC<CustomStatusBarProps> = ({
+  customBackgroundColor = "white",
+  ...props
+}) => {
+  return <RNStatusBar {...props} backgroundColor={customBackgroundColor} />;
+};
+
 export default function App() {
+  const statusBarHeight =
+    Platform.OS === "android" ? RNStatusBar.currentHeight ?? 0 : 0;
+
   return (
     <QueryClientProvider client={queryClient}>
-      <StatusBar style="light" />
+      <CustomStatusBar barStyle="light-content" translucent />
       <LinearGradient
         colors={["#5eead4", "#10b981"]}
         start={{ x: 0, y: 0 }}
         style={{ flex: 1 }}
         end={{ x: 1, y: 0 }}
       >
-        <SafeAreaView style={{ flex: 1 }}>
+        <SafeAreaView
+          className="flex-1 bg-transparent"
+          style={{
+            paddingTop: statusBarHeight + 5,
+          }}
+        >
           <RickAndMorty />
         </SafeAreaView>
       </LinearGradient>
